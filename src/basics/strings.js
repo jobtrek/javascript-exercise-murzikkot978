204f18a22c773379849258aa19272ae9f41855de
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-11-12 14:40:58 +0100

    feat: basics strings

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -8,4 +8,25 @@
  */
 export function findAndReplacePreservingCase(needle, haystack, newWord) {
   // Write your code here
+  if (typeof newWord !== 'string' || typeof needle !== 'string') {
+    throw new Error("...")
+  }
+
+  return haystack.replaceAll(new RegExp(needle, 'gi'), (match) => {
+    let result = ''
+    let transformed = newWord.slice(0, match.length)
+    
+
+    for (let i = 0; i < match.length; i++) {
+      const orig = match[i]
+      const transform = transformed[i] || ''
+      if (orig === orig.toUpperCase()) {
+        result += transform.toUpperCase()
+      }else{
+        result += transform.toLowerCase()
+      }
+    }
+
+    return result
+  })
 }
