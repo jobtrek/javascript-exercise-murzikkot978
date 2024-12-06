3a831ab0f3f8b218b91bb925bc95a2a01aa15f3b
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-12-06 11:28:53 +0100

    feat: inputs is finished

--- src/events/inputs.js
+++ src/events/inputs.js
@@ -4,7 +4,14 @@
  * If input his empty, you should not trigger the alert
  */
 export function displayInputContentInAlertOnEnterKey() {
-  // Write your code here
+    // Write your code here
+    const input = document.querySelector("#write-some-text")
+    input.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            const mess = input.value.trim()
+            alert(mess)
+        }
+    })
 }
 
 /**
@@ -13,13 +20,38 @@ export function displayInputContentInAlertOnEnterKey() {
  * the text should be added to a list of elements with id "list".
  */
 export function addElementsInListOnEnterKey() {
-  // Write your code here
+    // Write your code here
+    const input = document.querySelector("#list-input")
+    const output = document.querySelector("#list")
+
+    function addtolist() {
+        const text = input.value.trim()
+        if (text) {
+            const li = document.createElement("li")
+            li.textContent = text
+            output.appendChild(li)
+            input.value = ''
+        }
+
+    }
+
+    input.addEventListener("keydown", function (event) {
+        if (event.key === 'Enter') {
+            addtolist()
+        }
+    })
+    input.addEventListener("blur", addtolist)
 }
+
 
 /**
  * Add functionalities to the list. Now, when you click on one of the li, the element should be removed.
  * Use the same list as the previous exercise. "#list"
  */
 export function removeElementsFromListWhenClicked() {
-  // Write your code here
+    // Write your code here
+    const output = document.querySelector("#list")
+    output.addEventListener("click", (event) => {
+        event.target.remove()
+    })
 }
